c69efd219eee22b56f53ebabd0aaad4c918dbf36
Serhii <serikgorbanev@gmail.com>
2023-08-13 08:15:55 +0200

    Added Project

---
+++ Assets/Scripts/SpacecraftPart.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpacecraftPart : MonoBehaviour
+{
+    [SerializeField] RelativeJoint2D topJoint;
+    [SerializeField] RelativeJoint2D bottomJoint;
+    [SerializeField] RelativeJoint2D leftJoint;
+    [SerializeField] RelativeJoint2D rightJoint;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
